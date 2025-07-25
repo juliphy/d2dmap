3b73659e5ee8313c50ee2d31bd70406cbd280bf8
Sviatoslav Senchuk <juliphyy@gmail.com>
2025-07-25 12:24:28 +0200

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -18,3 +18,14 @@ export async function POST(req: NextRequest) {
   await prisma.user.create({ data: { name, email, password } })
   return NextResponse.json({ password: passwordPlain })
 }
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions)
+
+  if (!session || session.user.role !== 'ADMIN') {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
+
+  const users = await prisma.user.findMany()
+  return NextResponse.json(users)
+}
