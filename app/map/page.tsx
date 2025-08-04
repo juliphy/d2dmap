205c5f1ae131679fe704e4d66e2e138ac0a4b457
Sviatoslav Senchuk <juliphyy@gmail.com>
2025-08-04 16:52:44 +0200

    refactor: add map route for zone navigation

---
+++ app/map/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { SessionProvider } from "next-auth/react";
+import MapPage from "../MapPage";
+import { Suspense } from "react";
+
+export default function MapRoute() {
+  return (
+    <SessionProvider>
+      <Suspense><MapPage /></Suspense>
+    </SessionProvider>
+  );
+}
